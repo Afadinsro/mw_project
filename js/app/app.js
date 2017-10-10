b566ec02017a557fa9f58c909d1ef743e6ceb1d0
Afadinsro <mills.josef@gmail.com>
2017-10-10 11:44:42 +0000

    inclas quiz to display Ashesi departments

--- js/app/app.js
+++ js/app/app.js
@@ -7,9 +7,3 @@
 var startApp = function() {
   // alert('started');
 };
-
-function toggleSearch(){
-    var searchBar = document.getElementByID("search");
-    //searchBar.addClass("showSearchBar");
-    var searchIcon = document.getElementByID("searchIcon");
-}
